6536131619c675a0aff045f2918310b05e3726e4
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-14 16:19:20 +0100

    Numero di dipartimenti

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -33,7 +33,8 @@ FROM `degrees`
 WHERE `level` LIKE 'magistrale';
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-
+SELECT COUNT(name) AS 'Numero totale di dipartimenti'
+FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
 
