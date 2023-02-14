414d1b8d0443ed952e1474f90518eedc11cb9f8f
FedericoGuadagnoli <guadagnoli1989@gmail.com>
2023-02-14 17:08:30 +0100

    Correggo il metodo studenti che hanno più 30 anni

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -11,9 +11,9 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
-SELECT `name`,`surname`,`date_of_birth`
-FROM `students`
-WHERE YEAR(`date_of_birth`) < 1993
+SELECT `name`, `surname`, `date_of_birth` 
+FROM students
+WHERE `date_of_birth` < DATE_SUB(CURDATE(), INTERVAL 30 YEAR);
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT *
